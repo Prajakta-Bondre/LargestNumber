8c1fa1dfa71335ce1a470649eb53d754ee688f30
Prajakta-Bondre <radhabondre20@gmail.com>
2021-09-25 11:30:37 +0530

    find out the largest no among three

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            // set the value of the three numbers
+            num1 = 30;
+            num2 = 70;
+            num3 = 50;
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one is the largest!\n");
+                }
+                else
+                {
+                    Console.Write("Number three is the largest!\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("Number two is the largest!\n");
+            else
+                Console.Write("Number three is the largest!\n");
+        }
+    }
+}
+        
+    
+
